f08167cb026c6bbba43c8e4fae0cf0ac7a0cc88b
Nicolas <bircni@icloud.com>
2024-07-05 15:56:15 +0200

    Adjust examples

--- examples/multiple_variants.rs
+++ examples/multiple_variants.rs
@@ -1,12 +1,11 @@
+use egui::ScrollArea;
 use egui_phosphor::{bold, fill, light, regular, thin};
 
 fn main() {
     eframe::run_native(
         "egui-phosphor demo",
-        eframe::NativeOptions {
-            ..Default::default()
-        },
-        Box::new(|cc| Box::new(Demo::new(cc))),
+        Default::default(),
+        Box::new(|cc| Ok(Box::new(Demo::new(cc)))),
     )
     .unwrap();
 }
@@ -70,26 +69,43 @@ impl Demo {
 
 impl eframe::App for Demo {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let examples = [
+            ("phosphor-thin", thin::FILE_CODE),
+            ("phosphor-light", light::FILE_CODE),
+            ("phosphor", regular::FILE_CODE),
+            ("phosphor-bold", bold::FILE_CODE),
+            ("phosphor-fill", fill::FILE_CODE),
+        ];
         egui::CentralPanel::default().show(ctx, |ui| {
-            for (family, icon) in [
-                ("phosphor-thin", thin::FILE_CODE),
-                ("phosphor-light", light::FILE_CODE),
-                ("phosphor", regular::FILE_CODE),
-                ("phosphor-bold", bold::FILE_CODE),
-                ("phosphor-fill", fill::FILE_CODE),
-            ] {
-                ui.heading(family);
-                egui::Frame::canvas(ui.style()).show(ui, |ui| {
-                    for size in [16.0, 32.0, 48.0] {
-                        let demo_text = format!("FILE_CODE {icon}");
-                        ui.label(
-                            egui::RichText::new(&demo_text)
-                                .family(egui::FontFamily::Name(family.into()))
-                                .size(size),
-                        );
-                    }
-                });
-            }
+            ScrollArea::vertical().id_source("loop").show(ui, |ui| {
+                let mut iter = examples.iter();
+                while let Some((family, icon)) = iter.next() {
+                    ui.horizontal(|ui| {
+                        show_demo_tile(ui, family, icon);
+                        if let Some((next_family, next_icon)) = iter.next() {
+                            show_demo_tile(ui, next_family, next_icon);
+                        }
+                    });
+                }
+            });
         });
     }
+}
+
+fn show_demo_tile(ui: &mut egui::Ui, family: &str, icon: &str) {
+    ui.vertical(|ui| {
+        ui.heading(family);
+        egui::Frame::canvas(ui.style()).show(ui, |ui| {
+            ui.vertical(|ui| {
+                for size in [16.0, 32.0, 48.0] {
+                    let demo_text = format!("{} {icon}", family, icon = icon);
+                    ui.label(
+                        egui::RichText::new(&demo_text)
+                            .family(egui::FontFamily::Name(family.into()))
+                            .size(size),
+                    );
+                }
+            });
+        });
+    });
 }
